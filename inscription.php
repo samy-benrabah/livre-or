d7628ffbe932f503c7576f83431aa636e3a244a5
samy <samy>
2020-11-20 12:00:55 +0100

    fusion php samy et html labridou

---
+++ inscription.php
@@ -0,0 +1,6 @@
+<?php
+// Faire ma connexion avec la base de donnée
+$db = mysqli_connect('localhost', 'root', '', 'utilisateurs')
+//  Connexion si les valeurs entrée sont les memes que dans la base de donnée
+
+?>
