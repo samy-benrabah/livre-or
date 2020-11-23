0c2fbf4be46e5b15188dae81805ba78121d13c46
Samir B <samir.bensalma@laplateforme.io>
2020-11-23 16:36:36 +0100

    Push fin lundi

--- commentaire.php
+++ commentaire.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Manag€uro</title>
 </head>
 
@@ -23,21 +23,12 @@
             <a href="commentaire.php">Les Commentaires</a>
         </nav>
     </header>
-
     <main>
-        <?php
-        $wrong='';
-        //if (isset($_SESSION['login'])){
-          //  $connexion = mysqli_connect('localhost','root','','livreor');
-            //$requete = mysqli_query($connexion, 'SELECT * FROM utilisateurs');
-
-        ?>
         <section class="section1">
             <div>
                 <form action="" method="post">
                     <h3 id="red">MAN<span id="yellow">AGE</span><span id="green">URO</span></h3>
                     <h4>Commentaire</h4>
-                    <span id="error"><?php echo $wrong ?></span>
                     <br>
                     <label for="text"></label>
                     <textarea name="text" id="text" cols="30" rows="10" placeholder="Rédiger un commentaire"></textarea>
@@ -46,57 +37,34 @@
 
                     <?php
                     $connexion = mysqli_connect('localhost','root','','livreor');
-                    $query = "SELECT id, login FROM utilisateurs";
+                    $query = "SELECT id FROM utilisateurs";
                     $requete = mysqli_query($connexion, $query);
                     $allresult=mysqli_fetch_assoc($requete);
-                    var_dump($allresult);
                     $id = $allresult['id'];
-                    $login = $allresult['login'];
                     $date = date('Y-m-d H:i:s');
 
                     if (empty($_POST['valider'])){
                     echo "compléter votre commentaire et cliquez sur valider";
                     }
-                    elseif ($_POST['valider']){
+
+                    if (isset($_POST['valider'])){
                         if(!empty(trim($_POST['text']))){
+                            var_dump($_POST['text']);
                             $commentaire = $_POST['text'];
                             var_dump($commentaire);
-                            $query1 = "INSERT INTO commentaires( commentaire, id_utilisateur, date) VALUES ('$commentaire', '$id', '$date')";
+                            $query1 = "INSERT INTO `commentaires`( commentaire, id_utilisateur, date) VALUES ('$commentaire', '$id', '$date')";
                             $requete1 = mysqli_query($connexion, $query1);
-                            echo "Merci votre commentaire à bien été ajouté:" . '<br>';
-                            echo $commentaire;
+                            var_dump($connexion);
+                            var_dump($requete1);
+                            echo "Merci votre commentaire à bien été ajouté";
                         } else echo "Merci de compléter le champ commentaires";
-                    }
+                    } else echo "commentaire non ajouté";
+
                     ?>
                 </form>
             </div>
         </section>
-            <?php
-            $connexion = mysqli_connect('localhost','root','','livreor');
-            $query = "SELECT id, login FROM utilisateurs";
-            $requete = mysqli_query($connexion, $query);
-            $allresult=mysqli_fetch_assoc($requete);
-            var_dump($allresult);
-            $id = $allresult['id'];
-            $login = $allresult['login'];
-            $date = date('Y-m-d H:i:s');
-
-            if ($_POST['submit']){
-                    if($_POST['text']){
-                    var_dump($_POST['text']);
-                    $commentaire = $_POST['text'];
-                    $query1 = "INSERT INTO `commentaires`( `commentaire, id_utilisateur, date`) VALUES ('$commentaire', '$id', '$date')";
-                    $requete = mysqli_query($connexion, $query1);
-                    echo "Merci votre commentaire à bien été ajouté";
-                } echo "Merci de compléter le champ commentaires";
-                } echo "commentaire non ajouté";
-
-            var_dump($commentaire);
-
-            ?>
-
-
-    </main>
+     </main>
     <footer>
         <p>Copyright © All right reserved</p>
     </footer>
