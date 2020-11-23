1adc78434ef061c13178d1337c0048f33a692d8a
Samir B <samir.bensalma@laplateforme.io>
2020-11-23 16:37:16 +0100

    Push fin lundi

--- livre-or.php
+++ livre-or.php
@@ -1,15 +1,17 @@
 <?php
-
 session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "livreor";
 $sql = mysqli_connect($servername, $username, $password, $dbname);
+$query = mysqli_query($sql, 'SELECT commentaire, date FROM `commentaires` ORDER BY date DESC');
+$all_result = mysqli_fetch_all($query);
 
 if (isset($_SESSION['login'])) {
     $coucou = 'Coucou ' . $_SESSION['login'] . ' n\'hésite pas à laisser un commentaire';
-    $connecter = '<a href="profil.php">Mon Profil</a>';
+    $connecter = '
+                <a href="profil.php">Mon Profil</a>';
 }
 else $deconnecter = '<a href="inscription.php">Inscription</a>
                     <a href="connexion.php">Connexion</a>';
@@ -34,45 +36,32 @@ else $deconnecter = '<a href="inscription.php">Inscription</a>
         </div>
         <nav>
             <a href="index.php">Accueil</a>
-            <?php 
-                echo $deconnecter;
-                echo $connecter;
-            ?>
             <a href="commentaire.php"><u><b>Les Commentaires</b></u></a>
         </nav>
     </header>
-
     <main>
         <h3 class="h3" style="text-align: center; margin-top: 40px;" id="red">Comm<span id="yellow">ent</span><span id="green">aire</span></h3>
         <a href="commentaire.php"><p style="text-align: center; margin-top: 20px;"><?php echo $coucou ?></p></a>
         <section class="section4">
             <div class="comment">
-                <h3>MOMO</h3>
-                <br>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam voluptates facilis modi perspiciatis molestiae enim neque nobis, dolores sint recusandae minus architecto optio illum, doloremque, eos ea amet consectetur obcaecati.</p>
-                <br>
-                <h4>Le: 23/09/2020</h4>
-            </div>
-            <div class="comment">
-                <h3>MOMO</h3>
-                <br>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam voluptates facilis modi perspiciatis molestiae enim neque nobis, dolores sint recusandae minus architecto optio illum, doloremque, eos ea amet consectetur obcaecati.</p>
-                <br>
-                <h4>Le: 23/09/2020</h4>
-            </div>
-            <div class="comment">
-                <h3>MOMO</h3>
-                <br>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam voluptates facilis modi perspiciatis molestiae enim neque nobis, dolores sint recusandae minus architecto optio illum, doloremque, eos ea amet consectetur obcaecati.</p>
-                <br>
-                <h4>Le: 23/09/2020</h4>
-            </div>
-            <div class="comment">
-                <h3>MOMO</h3>
-                <br>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam voluptates facilis modi perspiciatis molestiae enim neque nobis, dolores sint recusandae minus architecto optio illum, doloremque, eos ea amet consectetur obcaecati.</p>
-                <br>
-                <h4>Le: 23/09/2020</h4>
+                <table>
+                    <tr>
+                    <th>Commentaire</th>
+                    <th>Date</th>
+                    </tr>
+                    <tbody>
+                <?php
+                for($i=0;isset($all_result[$i]);$i++){
+                    $j=0;
+                    echo "<tr>";
+                    while(isset($all_result[$i][$j])){
+                        echo "<td>" . $all_result[$i][$j] . "</td>";
+                        $j++;
+                    }echo "</tr>";
+                }
+                ?>
+                    </tbody>
+                </table>
             </div>
         </section>
     </main>
